f8474a73d95e4a3bf2dd29fe6bd4bd6bd4aa9037
gavin100305 <gavinsoares200510@gmail.com>
2025-03-22 18:51:44 +0530

    Influencer page done

--- frontend/src/pages/Analysis.jsx
+++ frontend/src/pages/Analysis.jsx
@@ -4,16 +4,12 @@ import Spotlight from "../components/Spotlight";
 import Navbar from "../components/Navbar";
 import Search from "../components/Search";
 import RomFooter from "../components/RomFooter";
-import InfluencerList from "../components/InfluencerList";
-import Search from "../components/Search";
-import RomFooter from "../components/RomFooter";
-import Header from "../components/Header";
 
 const Analysis = () => {
   return (
     <div className="min-h-screen bg-gradient-to-br from-purple-50 to-pink-50 font-sans text-gray-900">
       <Navbar />
-      
+
       <div className="container mx-auto px-4 py-8 max-w-4xl">
         <Search />
       </div>
