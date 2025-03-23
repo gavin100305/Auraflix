9876a27e4ca0187d497727d532c30220a32ac3a6
Romeiro <theromeirofernandes@gmail.com>
2025-03-23 15:57:42 +0530

    Added fe url

--- ai/fetch.py
+++ ai/fetch.py
@@ -29,7 +29,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],  
+    allow_origins=["https://influenceiq-nine.vercel.app"],  
     allow_credentials=True,
     allow_methods=["*"], 
     allow_headers=["*"],  
